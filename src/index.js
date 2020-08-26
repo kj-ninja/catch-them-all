6904d600d99b3b3a6abbe8ce6b45c3ed49c8659e
kj-ninja <jakubiak.krzy@gmail.com>
2020-08-26 16:56:00 +0200

    modified routes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/kj-ninja.github.io/">
+            <BrowserRouter>
                     <App/>
             </BrowserRouter>
         </Provider>
